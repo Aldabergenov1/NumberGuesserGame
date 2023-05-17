9ad2b4eb6253f4ba81b7cc17b2100481f4ea3a7b
Aldabergenov1 <elliotaldersson333@gmail.com>
2023-05-17 16:52:06 +0600

    Replay option is added

--- Number_Guesser.py
+++ Number_Guesser.py
@@ -1,6 +1,33 @@
 import random
 import time
 
+def user_guesser():
+    print('Случайное число задано, теперь, попробуйте его угадать! После каждой \
+неудачной попытки, вам будет дана подсказка, больше ли ваше предпологаемое число, или меньше.')
+    targeted_number = random.randint(lower_limit, upper_limit + 1)
+    guessed_number = 0
+    while guessed_number != targeted_number:
+        guessed_number = int(input())
+        if guessed_number > targeted_number:
+            print('Неверно, искомое число меньше того, что вы ввели: ')
+        elif guessed_number < targeted_number:
+            print('Неверно, искомое число больше того, что вы ввели: ')
+        else:
+            print('Вы угадали! Так держать!')
+
+def computer_guesser():
+    print('Загадайте число! Сейчас я попробую его отгадать.', 'У вас есть 5 секунд...', sep = '\n')
+    guessed_number = random.randint(lower_limit, upper_limit)
+    time.sleep(5)
+    answear = 0
+    while answear != 1:
+        print(f'Вы загадали число {guessed_number}?', '1 - Да', '2 - Нет')
+        answear = int(input())
+        guessed_number = random.randint(lower_limit, upper_limit)
+        if answear == 1:
+            print('Ура! Приятно было с вами поиграть!')
+
+
 print('Здравствуйте! Это игра "угадай число", пожалуйста, выберите, кто будет угадывать число: ')
 print('1 - пользователь', '2 - компьютер', sep = '\n')
 choise = int(input())
@@ -17,32 +44,22 @@ while upper_limit <= lower_limit:
     print('Ошибка! Введите корректное значение!')
     lower_limit = int(input('Нижняя граница диапазона: '))
     upper_limit = int(input('Верхняя граница диапазона: '))
-
-print('Случайное число задано, теперь, попробуйте его угадать! После каждой неудачной попытки, вам будет дана подсказка, больше ли ваше предпологаемое число, или меньше.')
-
-targeted_number = random.randint(lower_limit, upper_limit + 1)
-
-if choise == 1:
-    guessed_number = 0
-    while guessed_number != targeted_number:
-        guessed_number = int(input())
-        if guessed_number > targeted_number:
-            print('Неверно, искомое число меньше того, что вы ввели: ')
-        elif guessed_number < targeted_number:
-            print('Неверно, искомое число больше того, что вы ввели: ')
-        else:
-            print('Вы угадали! Так держать!')
-
-elif choise == 2:
-    print('Загадайте число! Сейчас я попробую его отгадать.', 'У вас есть 5 секунд...', sep = '\n')
-    guessed_number = random.randint(lower_limit, upper_limit)
-    time.sleep(5)
-    answear = 0
-    while answear != 1:
-        print(f'Вы загадали число {guessed_number}?', '1 - Да', '2 - Нет')
-        answear = int(input())
-        guessed_number = random.randint(lower_limit, upper_limit)
-        if answear == 1:
-            print('Ура! Приятно было с вами поиграть!')
-else: 
-    print('Ошибка! Введите корректное значение!')
+while True:
+    if choise == 1:
+        user_guesser()
+        print('Хотите сыграть ещё?', '1 - Да', '2 - Нет', sep = '\n')
+        contin = int(input())
+        if contin == 2:
+            print('Приятно было с вами поиграть!')
+            break
+        elif contin == 1:
+            continue
+    elif choise == 2:
+        computer_guesser()
+        print('Хотите сыграть ещё?', '1 - Да', '2 - Нет', sep = '\n')
+        contin = int(input())
+        if contin == 2:
+            print('Приятно было с вами поиграть!')
+            break
+        elif contin == 1:
+            continue
